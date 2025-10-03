20f6e59415f91a2756257ffd55c62462fe45b8de
Astrid Konicki <astrid.konicki@mymail.champlain.edu>
2025-10-02 23:41:54 -0400

    Add example of mal program

--- output.cpp
+++ output.cpp
@@ -1,10 +1,15 @@
-#include <fstream>
-#include <vector>
-#include <iostream>
 #include <chrono>
+#include <windows.h>
+#include <thread>
+#include <iostream>
 
 int main() {
-	std::cout << "Hello World" << std::endl;
-	std::cin.get();
-	return 0;
+
+    std::cout << "Creating windows popup..." << std::endl;
+    while(true) {
+        MessageBox(nullptr,"This is an example of a generated malware executable running!","Malware Analysis Language", MB_OK);
+        Sleep(10 * 1000);
+    }
+
+    return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,10 @@
 use std::collections::HashSet;
 use std::fs;
+use regex::Regex;
 
 fn main() {
 
-    let mut new_set = HashSet::new();
-
-    new_set.insert(5);
-    new_set.insert(6);
-    new_set.insert(7);
-    new_set.insert(8);
-
-    println!("{:?}", new_set);
-
-    match output_to_file() {
+    match generate_malware() {
         Ok(()) => { println!("done"); }
         Err(e) => { println!("failed: {}", e); }
     }
@@ -20,25 +12,49 @@ fn main() {
 
 type Include = String;
 
-fn output_to_file() -> std::io::Result<()> {
-    let mut includes_set: HashSet<Include> = HashSet::new();
+fn generate_malware() -> std::io::Result<()> {
 
-    includes_set.insert("<iostream>".to_string());
-    includes_set.insert("<fstream>".to_string());
-    includes_set.insert("<vector>".to_string());
-    includes_set.insert("<chrono>".to_string());
+    let data =  read_malware_program("./.mal-scripts/example.mal").unwrap();
 
-    let includes = includes_set.iter()
+    println!("data: {:?}", data);
+
+    let includes: HashSet<Include> = HashSet::from(["<windows.h>", "<iostream>", "<chrono>", "<thread>"].map(|s| s.to_string()));
+
+    let print_re = Regex::new(r#"print\([ \t\n]*"(.+)"[ \t\n]*\)"#).unwrap();
+    let windows_popup_re = Regex::new(r#"create_windows_popup\([ \t\n]*"(.+)",[ \t\n]*"(.+)"[ \t\n]*\)"#).unwrap();
+    let sleep_re = Regex::new(r#"sleep\([ \t\n]*([0-9]+)[ \t\n]*\)"#).unwrap();
+
+    let cpp_script: String = data.replace("start", "main");
+
+    println!("print statement found: {}", print_re.is_match(&cpp_script));
+    println!("windows popup statement found: {}", windows_popup_re.is_match(&cpp_script));
+    println!("sleep statement found: {}", sleep_re.is_match(&cpp_script));
+
+    let after_print_replace = print_re.replace_all(&cpp_script, "std::cout << \"$1\" << std::endl");
+    let after_win_popup_replace = windows_popup_re.replace_all(&after_print_replace,
+       "MessageBox(nullptr,\"$1\",\"$2\", MB_OK)");
+    let after_sleep_replace = sleep_re.replace_all(&after_win_popup_replace, "Sleep($1 * 1000)").to_string();
+
+    output_to_file(includes, after_sleep_replace)
+}
+
+fn read_malware_program(file_name: &str) -> Result<String, String> {
+    match fs::read_to_string(file_name) {
+        Ok(content) => { Ok(content) }
+        Err(e) => { Err(format!("Error reading file: {}", e)) }
+    }
+}
+
+fn output_to_file(includes: HashSet<Include>, main_content: String) -> std::io::Result<()> {
+
+    let formatted_includes = includes.iter()
         .map(|x| format!("#include {}", x.to_string()))
         .collect::<Vec<String>>()
         .join("\n");
 
-    println!("{}", includes);
+    // println!("{}", formatted_includes);
 
-    let output = includes + "\n\nint main() {\n\tstd::cout << \"Hello World\" << std::endl;\n\treturn 0;\n}\n";
+    let output = formatted_includes + "\n\n"+  main_content.as_str();
 
-    fs::write("output.cpp", output)?;
-
-
-    Ok(())
+    fs::write("output.cpp", output)
 }
